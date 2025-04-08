1259718e431b2cf88f25d64bec60271422a1a09c
Mohak Jain <mohakj500@gmail.com>
2025-04-08 20:27:48 +0530

    increase timeout

--- better-notes-frontend/src/logic/AxiosInstance.tsx
+++ better-notes-frontend/src/logic/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
     baseURL : 'https://better-notes.onrender.com/',
-    timeout : 1000,
+    timeout : 5000,
     headers : {
         'Content-Type' : 'application/json',
     }
